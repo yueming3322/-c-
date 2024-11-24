56b8387bac195353841575475959541e4d2a060e
yueming3322 <3312398094@qq.com>
2024-11-24 22:13:19 +0800

    初学c语言

---
+++ 002/002/01.c
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//
+//int main()
+//{
+//	int a = (int)3.14;
+//	printf("%d\n",a);
+//
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	int a = 10;
+//	if (a == 3)   //a = 3复制；a == 3条件等于
+//	{
+//		printf("hehe\n");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 20;
+//	if (a && b)  //&&同时满足，||其一满足
+//	{
+//		printf("hehe\n");//一假则假
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 30;
+//	int b = 20;
+//
+//	int r = a > b ? a : b;
+//	printf("%d\n", r);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 0;
+//
+//	int d = (c = a - 2, a = b + c, c - 3);
+//	//逗号表达式只输出最后一个数！
+//	printf("%d\n", d);
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8 };
+//	int n = 3;
+//	arr[n] = 3;
+//
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	return x+y;
+//}
+//
+//int main()
+//{
+//	int sum = Add(2, 3);
+//	printf("%d\n",sum);
+//
+//	return 0;
+//}
+
+
