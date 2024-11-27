5e2180dff9d2b684a1e078c60c4cc5b181313fc8
yueming3322 <3312398094@qq.com>
2024-11-27 22:05:17 +0800

    结构体

---
+++ 005/005/01.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//	//不管是什么类型的指针，都是在创建指针变量
+//	//指针变量是用来存放地址的
+//	//指针变量的大小取决于一个地址存放的时间需要多大空间
+//	//32位机器上的地址：32bit位的  -4byte，所以指针大小就是4
+//	//64位机器上的地址：64bit位的  -8byte，所以指针大小就是8
+//
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(float*));
+//	printf("%d\n", sizeof(double*));
+//
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    //int c = a / b; int d = a % b;
+//    printf("%d %d", a / b, a % b);
+//
+//    return 0;
+//}
