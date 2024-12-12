35e74dfeb52a60fc934167f584ac6b322526a66d
yueming3322 <3312398094@qq.com>
2024-12-12 17:05:12 +0800

    补充

--- 009/009/03.c
+++ 009/009/03.c
@@ -1,5 +1,5 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
 
 //int main()
 //{
@@ -92,18 +92,19 @@
 //}
 
 
-int main()
-{
-	for (int i = 1; i <= 9; i++)
-	{
-		for (int j = 1; j <= i; j++)
-		{
-			printf("%d * %d = %d\t", j, i, i * j);
-		
-		}
-		printf("\n");
-	}
-	//九九乘法表
+//int main()
+//{
+//	for (int i = 1; i <= 9; i++)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d * %d = %d\t", j, i, i * j);
+//		
+//		}
+//		printf("\n");
+//	}
+//	//九九乘法表
+//
+//	return 0;
+//}
 
-	return 0;
-}
