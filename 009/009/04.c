a6c3510253f4b454d4001a204ada87d9053df03a
yueming3322 <3312398094@qq.com>
2024-12-12 22:35:38 +0800

    1

--- 009/009/04.c
+++ 009/009/04.c
@@ -1,31 +1,31 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int countA = 0;
-//
-//	for (int i = 2; i <= 100; i++)
-//	{
-//		int count = 0;
-//		for (int j = 2; j < i; j++)
-//		{
-//			if (i % j == 0)
-//			{
-//				count++;
-//				break;					//提高效率
-//			}
-//		}
-//		if (count == 0)
-//		{
-//			countA++;
-//			printf("%d是质数\n", i);
-//		}
-//		else
-//		{
-//			printf("%d不是质数\n", i);
-//		}
-//	}
-//	printf("%d\n", countA++);					//区间质数的个数
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int countA = 0;
+
+	for (int i = 2; i <= 100; i++)
+	{
+		int count = 0;
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				count++;
+				break;					//提高效率
+			}
+		}
+		if (count == 0)
+		{
+			countA++;
+			printf("%d是质数\n", i);
+		}
+		else
+		{
+			printf("%d不是质数\n", i);
+		}
+	}
+	printf("%d\n", countA++);					//区间质数的个数
+	return 0;
+}
